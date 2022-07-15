41eac09feb614b8d6e08b46372b5979f02058e01
Your Name <you@example.com>
2022-07-15 13:52:27 +0100

    navbar almost done

--- news/src/Components/Navbar/Navbar.jsx
+++ news/src/Components/Navbar/Navbar.jsx
@@ -9,6 +9,21 @@ export default function Navbar() {
           <a className="logo" href="#">
             news.
           </a>
+          <a className="item-container" href="#">
+            Home
+          </a>
+          <a className="item-container" href="#">
+            TOPIC A
+          </a>
+          <a className="item-container" href="#">
+            TOPIC B
+          </a>
+          <a className="item=container" href="#">
+            TOPIC C
+          </a>
+        </div>
+        <div className="right">
+          <input type="text" className="search-bar" placeholder="Search..." />
         </div>
       </div>
     </div>
