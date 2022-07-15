34da7e521ff5a5b7c9e23e21533d29d7fcbe9752
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-07-15 10:40:18 +0100

    fetched data

--- news/src/App.js
+++ news/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [articles, setArticles] = useState([]);
+  const apiKey = process.env.REACT_APP_API_KEY_NEWS;
+
+  // Fetching Data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedData = await fetch(
+          `https://newsdata.io/api/1/news?apikey=pub_9231540a963fa511d9084f05a7cea240d392&language=en`
+        );
+        const data = await fetchedData.json();
+        console.log(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
